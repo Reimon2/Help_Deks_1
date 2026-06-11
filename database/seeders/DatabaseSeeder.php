8cfd45d1e331a1741b485820424b758219bdd04d
Reimon2 <medinajoseramon25@gmail.com>
2026-06-11 19:47:32 -0400

    Eliminada migracion vieja conflictiva de roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+$this->call([
+    UsuarioSeeder::class,
+]);
 
 class DatabaseSeeder extends Seeder
 {
